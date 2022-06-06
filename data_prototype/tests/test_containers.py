93d2ebb5b281e343a61dde45eeff1900abae8a54
Thomas A Caswell <tcaswell@gmail.com>
2022-06-06 00:47:03 -0400

    TST: add tests!

---
+++ data_prototype/tests/test_containers.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+import pytest
+
+
+from .. import containers
+
+
+@pytest.fixture
+def ac():
+    return containers.ArrayContainer(a=np.arange(5), b=np.arange(42, dtype=float).reshape(6, 7))
+
+
+def _verify_describe(container):
+    data, cache_key = container.query([0, 1, 0, 1], [100, 100], None, None)
+    desc = container.describe()
+
+    assert set(data) == set(desc)
+    for k, v in data.items():
+        assert v.shape == desc[k].shape
+        assert v.dtype == desc[k].dtype
+
+
+def test_array_describe(ac):
+    _verify_describe(ac)
+
+
+def test_array_ignore_query(ac):
+    data, cache_key = ac.query([0, 1, 0, 1], [100, 100], None, None)
+    data2, cache_key_2 = ac.query([0, 2, 0, 2], [10, 10], None, None)
+    assert cache_key == cache_key_2
+
+    for k in set(data) | set(data2):
+        assert np.all(data[k] == data2[k])
+
+
+def test_array_cache_stable(ac):
+    data, cache_key = ac.query([0, 1, 0, 1], [100, 100], None, None)
+    data2, cache_key_2 = ac.query([0, 1, 0, 1], [100, 100], None, None)
+    assert cache_key == cache_key_2
+
+    for k in set(data) | set(data2):
+        assert np.all(data[k] == data2[k])
+
+
+def test_array_cache_update(ac):
+    data, cache_key = ac.query([0, 1, 0, 1], [100, 100], None, None)
+    ac.update(**{k: v * 2 for k, v in data.items()})
+    data2, cache_key_2 = ac.query([0, 1, 0, 1], [100, 100], None, None)
+
+    assert cache_key != cache_key_2
+    for k in set(data) | set(data2):
+        assert np.all(2 * data[k] == data2[k])
+
+
+def test_array_no_new_keys(ac):
+    with pytest.raises(containers.NoNewKeys):
+        ac.update(d=[1, 2])
+
+
+@pytest.fixture
+def rc():
+    return containers.RandomContainer(a=(5,), b=(6, 7))
+
+
+def test_random_describe(rc):
+    _verify_describe(rc)
+
+
+def test_random_ustable_cache(rc):
+    _, cache_key = rc.query([0, 1, 0, 1], [100, 100], None, None)
+    _, cache_key2 = rc.query([0, 1, 0, 1], [100, 100], None, None)
+    assert cache_key != cache_key2
