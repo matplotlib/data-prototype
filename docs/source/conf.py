3938b84174445543564b3e81973b6d7c62dcf3fb
Thomas A Caswell <tcaswell@gmail.com>
2022-10-21 17:18:46 -0400

    MNT: fix doc warning + sush mypy

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -159,7 +159,10 @@ html_theme = "mpl_sphinx_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+
+html_theme_options = {
+    "logo": {}
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
