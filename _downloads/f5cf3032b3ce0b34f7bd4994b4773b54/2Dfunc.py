ad931a3da337b929c3022676bb83f73d2c597af8
ksunden <ksunden@users.noreply.github.com>
2024-08-16 18:06:40 +0000

    deploy: 7021e106aa1744548eebfb9c3ca007594fdc6800

---
+++ _downloads/f5cf3032b3ce0b34f7bd4994b4773b54/2Dfunc.py
@@ -0,0 +1,38 @@
+"""
+=====================
+A functional 2D image
+=====================
+
+A 2D image generated using :class:`.containers.FuncContainer` and
+:class:`wrappers.ImageWrapper`.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from data_prototype.wrappers import ImageWrapper
+from data_prototype.containers import FuncContainer
+
+from matplotlib.colors import Normalize
+
+
+fc = FuncContainer(
+    {},
+    xyfuncs={
+        "xextent": ((2,), lambda x, y: [x[0], x[-1]]),
+        "yextent": ((2,), lambda x, y: [y[0], y[-1]]),
+        "image": (
+            ("N", "M"),
+            lambda x, y: np.sin(x).reshape(1, -1) * np.cos(y).reshape(-1, 1),
+        ),
+    },
+)
+norm = Normalize(vmin=-1, vmax=1)
+im = ImageWrapper(fc, norm=norm)
+
+fig, ax = plt.subplots()
+ax.add_artist(im)
+ax.set_xlim(-5, 5)
+ax.set_ylim(-5, 5)
+fig.colorbar(im)
+plt.show()
